d0684e9727b7c4d8806e44be2957b7b925add37a
Yakudzito <aremcukzahar@gmail.com>
2025-06-30 22:46:10 +0300

    feat: add question shuffle function

--- Quizzamination/Views/MainWindow.xaml.cs
+++ Quizzamination/Views/MainWindow.xaml.cs
@@ -7,6 +7,19 @@ using System.Windows.Threading;
 
 namespace Quizzamination.Views
 {
+    public static class ListExtensions
+    {
+        private static readonly Random _random = new Random();
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                int j = _random.Next(i + 1);
+                (list[i], list[j]) = (list[j], list[i]);
+            }
+        }
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -22,7 +35,8 @@ namespace Quizzamination.Views
         public MainWindow()
         {
             InitializeComponent();
-            _questions = TestLoader.LoadFromFile("test1.json");
+            _questions = TestLoader.LoadFromFile("3.6.json");
+            _questions.Shuffle();
             ShowCurrentQuestion();
             StartTimer();
         }
